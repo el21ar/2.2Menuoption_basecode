2cc1120b7e902c56156c6d425fb79e18303c70a3
el21ar <>
2022-11-28 16:07:27 +0000

    Set  universal constants and call libraries

--- .ccls-cache/@home@runner@22Menuoptionbasecode/main.cpp
+++ .ccls-cache/@home@runner@22Menuoptionbasecode/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <cmath>
+#include <map>
+#include <string>
+#include <fstream>
+
+const float h = 6.626e-34;
+const int c = 3e8;
+const float e = 1.6e-19;
+
 
 int main() {
-  std::cout << "Hello World!\n";
+  
 }
